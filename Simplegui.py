ce9baca56123075fe550ba4f367313205b0eb35d
GRAZYHULU <alex.hartikka@gmail.com>
2021-10-03 20:38:39 +0300

    somethings

--- Simplegui.py
+++ Simplegui.py
@@ -1,42 +1,66 @@
 import os
 import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import TabGroup
 from AESEncryption import AESenc
 
 
 def FileEncryption(inputFile, password):
-    AESobject = AESenc()
-    AESobject.setPassword(password=password)
-    readFile = AESobject.readFile(inputFile)
-    encryptedMessage = AESobject.encryptMessage(readFile)
-    outFile = f"{inputFile}.enc"
-    AESobject.writeFile(outFile, encryptedMessage)
-    os.remove(inputFile)
+    try:
+        if not os.path.isfile(inputFile):
+            print("File not found")
+        
+        if not password:
+            print("Give password")
+
+        AESobject = AESenc()
+        AESobject.setPassword(password=password)
+        readFile = AESobject.readFile(inputFile)
+        encryptedMessage = AESobject.encryptMessage(readFile)
+        outFile = f"{inputFile}.enc"
+        AESobject.writeFile(outFile, encryptedMessage)
+        os.remove(inputFile)
+    except Exception:
+        pass
+        
 
 
 def FileDecryption(inputFile, password):
-    AESobject = AESenc()
-    AESobject.setPassword(password=password)
-    readFile = AESobject.readFile(inputFile)
-    encryptedMessage = AESobject.decryptMessage(readFile)
-    outFile = inputFile.rsplit(".", 1)[0]
-    AESobject.writeFile(outFile, encryptedMessage)
-    os.remove(inputFile)
+    try:
+        if not os.path.isfile(inputFile):
+            print("File not found")
+        if not password:
+            print("Give password")
+            
+        AESobject = AESenc()
+        AESobject.setPassword(password=password)
+        readFile = AESobject.readFile(inputFile)
+        encryptedMessage = AESobject.decryptMessage(readFile)
+        outFile = inputFile.rsplit(".", 1)[0]
+        AESobject.writeFile(outFile, encryptedMessage)
+        os.remove(inputFile)
+    except Exception:
+        pass
 
 
 def main():
     sg.theme('Dark Blue 3')  # please make your creations colorful
 
-    layout = [[sg.Text('Filename')],
+    tab1_layout = [[sg.Text('Filename')],
               [sg.Input(), sg.FileBrowse()],
               [sg.Text("Password")],
               [sg.Input()],
-              [sg.Button("Encrypt"), sg.Button("Decrypt"), sg.Button("Exit")]]
+              [sg.Button("Encrypt"), sg.Button("Decrypt")]]
+
+    tab2_layout = [[sg.Text('Message')],
+              [sg.Multiline()]]
+
+    layout = [[sg.TabGroup([[sg.Tab('Tab 1', tab1_layout), sg.Tab('Tab 2', tab2_layout)]])],
+            [sg.Button("Exit")]]
 
     window = sg.Window('Get filename example', layout)
     while True:
 
         event, values = window.read()
-        print(event, values)
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
 
